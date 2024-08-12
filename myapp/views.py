9ae14e30b7fdbe6c3a4e044032a86ae3ed5d5a82
Mups1802 <101131367+Mups1802@users.noreply.github.com>
2024-08-12 13:57:52 +0200

    Add files via upload

---
+++ myapp/views.py
@@ -0,0 +1,17 @@
+
+from rest_framework import generics
+from .models import Temperature
+from .serializer import TemperatureSerializer
+
+class TemperatureDetailView(generics.RetrieveAPIView):
+    queryset = Temperature.objects.all()
+    serializer_class = TemperatureSerializer
+
+class TemperatureListView(generics.ListAPIView):
+    queryset = Temperature.objects.all()
+    serializer_class = TemperatureSerializer
+
+class TemperatureCreateAPIView(generics.CreateAPIView):
+    queryset = Temperature.objects.all()
+    serializer_class = TemperatureSerializer
+
