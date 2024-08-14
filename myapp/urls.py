a7e9b2fdc54fd2f1e17953c6850cdc02d0fc6d76
Mups1802 <chamamupelwa@gmail.com>
2024-08-14 13:33:09 +0200

    changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TemperatureDetailView,  TemperatureListView, TemperatureCreateAPIView, LatestTemperatureAPIView
+from .views import TemperatureDetailView,  TemperatureListView, TemperatureCreateAPIView, LatestTemperatureAPIView, temperature_monitor
 
 urlpatterns = [
     path('temperatures/', TemperatureListView.as_view(), name='temperature-list'),
     path('temperatures/<int:pk>/', TemperatureDetailView.as_view(), name='temperature-detail'),
     path('temperature/', TemperatureCreateAPIView.as_view(), name='temperature-create'),
     path('api/latest-temperature/', LatestTemperatureAPIView.as_view(), name='latest-temperature'),
+    path('temperature-monitor/', temperature_monitor, name='temperature-monitor')
 ]
