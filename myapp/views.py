7117a31c5f7de60f5182ad96700c95b1b165b734
Mups1802 <chamamupelwa@gmail.com>
2024-08-19 17:13:14 +0200

    add

--- myapp/views.py
+++ myapp/views.py
@@ -2,25 +2,36 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics
-from .models import Temperature
-from .serializer import TemperatureSerializer
+from .models import Temperature, Humidity
+from .serializer import TemperatureSerializer, HumiditySerializer
 
 class TemperatureDetailView(generics.RetrieveAPIView):
     queryset = Temperature.objects.all()
     serializer_class = TemperatureSerializer
+    queryset = Humidity.objects.all()
+    serializer_class = HumiditySerializer
 
 class TemperatureListView(generics.ListAPIView):
     queryset = Temperature.objects.all()
     serializer_class = TemperatureSerializer
+    queryset = Humidity.objects.all()
+    serializer_class = HumiditySerializer
 
 class TemperatureCreateAPIView(generics.CreateAPIView):
     queryset = Temperature.objects.all()
     serializer_class = TemperatureSerializer
+    queryset = Humidity.objects.all()
+    serializer_class = HumiditySerializer
     
 class LatestTemperatureAPIView(APIView):
     def get(self, request):
         latest_temperature = Temperature.objects.latest('id')
         return Response({"temperature": latest_temperature.data})
+class LatestHumidityTemperatureAPIView(APIView):
+    def get(self, request):
+        latest_humidity = Humidity.objects.latest('id')
+        return Response({"humidity": latest_humidity.data})
+
 
 def temperature_monitor(request):
     return render(request, 'temperature.html')
