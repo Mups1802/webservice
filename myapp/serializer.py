589bab4eeb55a819458deb06f343655a772fc65b
Mups1802 <chamamupelwa@gmail.com>
2024-08-19 17:22:22 +0200

    change

--- myapp/serializer.py
+++ myapp/serializer.py
@@ -5,9 +5,9 @@ from .models import Temperature, Humidity
 class TemperatureSerializer(serializers.ModelSerializer):
     class Meta:
         model = Temperature
-        fields = ['data']
+        fields = ['temperature']
 
 class HumiditySerializer(serializers.ModelSerializer):
     class Meta:
         model = Humidity
-        fields = ['data']
+        fields = ['humidity']
