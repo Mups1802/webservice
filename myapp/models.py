baf185e650945eba67dffe739dbf82c17c543798
Mups1802 <chamamupelwa@gmail.com>
2024-08-21 09:06:01 +0200

    commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-# Create your models here.
-class Temperature(models.Model):
-    data = models.CharField(max_length=20)
+class TemperatureReading(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    temperature = models.FloatField()
+    humidity = models.FloatField()
 
-class Humidity(models.Model):
-    data = models.CharField(max_length=20)
+    def __str__(self):
+        return f"{self.timestamp} - {self.temperature}°C - {self.humidity}%"
