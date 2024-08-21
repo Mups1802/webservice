431ed770f8ee155315d414831efbaaa9623fa076
Mups1802 <chamamupelwa@gmail.com>
2024-08-21 09:15:30 +0200

    commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from .views import TemperatureDetailView,  TemperatureListView, TemperatureCreateAPIView, LatestTemperatureAPIView, temperature_monitor, LatestHumidityAPIView
+from .views import TemperatureReadingList, TemperatureReadingDetail, TemperatureCreateAPIView, LatestTemperatureAPIView, temperature_monitor
 
 urlpatterns = [
-    path('temperatures/', TemperatureListView.as_view(), name='temperature-list'),
-    path('temperatures/<int:pk>/', TemperatureDetailView.as_view(), name='temperature-detail'),
-    path('temperature/', TemperatureCreateAPIView.as_view(), name='temperature-create'),
-    path('api/latest-temperature/', LatestTemperatureAPIView.as_view(), name='latest-temperature'),
+    path('temperatures/', TemperatureReadingList.as_view(), name='temperature-list'),
+    path('temperatures/<int:pk>/', TemperatureReadingDetail.as_view(), name='temperature-detail'),
     path('temperatures/create/', TemperatureCreateAPIView.as_view(), name='temperature-create'),
-    path('api/latest-humidity/', LatestHumidityAPIView.as_view(), name='latest-temperature'),
-    path('temperature-monitor/', temperature_monitor, name='temperature-monitor')
+    path('latest-temperature/', LatestTemperatureAPIView.as_view(), name='latest-temperature'),
+    path('', temperature_monitor, name='temperature-monitor'),
 ]
